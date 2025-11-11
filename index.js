97a39236ec793151b65f3fc94190670b69dae462
LishadWahid <lishadwahid@gmail.com>
2025-11-11 03:12:07 +0300

    updated

--- index.js
+++ index.js
@@ -49,6 +49,12 @@ async function run() {
             }
         })
 
+        app.get('/latest-listings', async (req, res) => {
+            const cursor = pawmartCollection.find().sort({_id: -1}).limit(6);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
